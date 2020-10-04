06a7a887cf57d4df38996016f7658c68124a8d54
Pablo Tobalina <pablotobalina@gmail.com>
2020-10-04 19:02:04 +0200

    Basic graph.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as shape from 'd3-shape';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,51 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'DependencyGraph';
+  curve: any = shape.curveLinear;
+  nodes = [
+    {
+      id: 'start',
+      label: 'scan'
+    }, {
+      id: '1',
+      label: 'Event#a'
+    }, {
+      id: '2',
+      label: 'Event#x'
+    }, {
+      id: '3',
+      label: 'Event#b'
+    }, {
+      id: '4',
+      label: 'Event#c'
+    }, {
+      id: '5',
+      label: 'Event#y'
+    }, {
+      id: '6',
+      label: 'Event#z'
+    }
+  ];
+
+  links = [
+    {
+      source: 'start',
+      target: '1'
+    }, {
+      source: 'start',
+      target: '2'
+    }, {
+      source: '1',
+      target: '3'
+    }, {
+      source: '2',
+      target: '4'
+    }, {
+      source: '2',
+      target: '6'
+    }, {
+      source: '6',
+      target: '5'
+    }
+  ];
 }
